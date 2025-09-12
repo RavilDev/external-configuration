2b01309c2386d91894843ca19f2c0c620e73871c
Safargulov Ravil <safargulov_ravil@mail.ru>
2025-09-12 22:03:51 +0500

    feat(code) implement external configuration pattern

---
+++ src/test/java/homework/externalconfiguration/ExternalConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.externalconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
